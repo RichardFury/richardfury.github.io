43c616098df4eabe24545023137786cc825279b1
Richard Fury <1397320399@qq.com>
2025-10-22 00:03:33 +0800

    Prepared for github pages:rocket:

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // GitHub Pages部署的base路径配置
+  // 如果你的仓库名称是'newprofile'，则保持这个配置
+  // 如果你使用自定义域名，可以设置为'/'
+  base: '/newprofile/'
 })
